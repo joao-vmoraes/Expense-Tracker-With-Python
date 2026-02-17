e787d68017955aedb92ef7c8af77563c329a5330
João Moraes <jvmmoraesdev@gmail.com>
2026-02-16 23:52:57 -0300

    feat: add _init_db

---
+++ app/database/_init_db.sql
@@ -0,0 +1,30 @@
+-- iNITIALIZATION OF THE DATABASE
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `nome` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO categorias (nome) VALUES
+	('🍔 Alimentação'),
+	('🚗 Transporte'),
+	('🎮 Lazer'),
+	('💊 Saúde'),
+	('🏠 Moradia'),
+	('📱 Outrossss');
+
+
+-- dados_db.Compra definition
+
+
+CREATE TABLE IF NOT EXISTS `compra` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `id_categoria` int unsigned NOT NULL,
+    `nome` varchar(30) NOT NULL,
+    `valor` decimal(8,2) NOT NULL,
+    `data` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `Compra_categorias_FK` (`id_categoria`),
+    CONSTRAINT `Compra_categorias_FK` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
